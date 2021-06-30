62e79ca7911dab5e3ddb4540861bf8333a9f6426
vanit4s <szymon.kowalsky@hotmail.com>
2021-06-30 10:25:30 +0100

    Added catalog functionality

--- app/src/components/Steps/Catalog.vue
+++ app/src/components/Steps/Catalog.vue
@@ -9,22 +9,24 @@
 			Now we’ll set up your catalog, which includes products, categories, tags and other parts of your store's information.
       You can choose to import sample products or skip this step if you already have catalog data or wish to add it later.
 		</template>
-		
-		<div class="mb-9">
-			<SuccessMessage>The database you have selected already has catalog data</SuccessMessage>
-		</div>
+
+    <ErrorMessage v-if="errorMessage">{{ errorMessage }}</ErrorMessage>
+
+<!--		<div class="mb-9">-->
+<!--			<SuccessMessage>The database you have selected already has catalog data</SuccessMessage>-->
+<!--		</div>-->
 
 		<div class="mb-9">
-			<ContentGroup id="retail" :selected="false" group="catalog">
+			<ContentGroup id="retail" v-model="setupData.project.catalog.type" group="catalog" value="import">
 
 				<template #title>
 					Import sample data
 				</template>
-				
+
 				<div class="mb-6">
 					<WarningMessage>Importing sample data will override any catalog data that already exists</WarningMessage>
 				</div>
-				
+
 				<div class="mb-6">
 					<label for="select-sample" class="mb-1">Select sample</label>
 					<select id="select-sample" name="select-sample">
@@ -34,15 +36,15 @@
 					</select>
 				</div>
 
-				<p class="mb-1 text-sm text-alphaLight-800">Sample preview;</p>
+				<p class="mb-1 text-sm text-alphaLight-800">Sample preview:</p>
 
 				<div class="grid grid-cols-4 gap-6">
-					
+
 					<div class="relative pb-full bg-alpha-300">
 						<PhotographIcon class="text-alphaLight-200 w-6 h-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
 					</div>
 					<div class="relative pb-full bg-alpha-300">
-						<PhotographIcon class="text-alphaLight-200 w-6 h-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />						
+						<PhotographIcon class="text-alphaLight-200 w-6 h-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
 					</div>
 					<div class="relative pb-full bg-alpha-300">
 						<PhotographIcon class="text-alphaLight-200 w-6 h-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
@@ -57,15 +59,15 @@
 		</div>
 
 		<div>
-			<ContentGroup id="skip-catalog" :selected="true" group="catalog">
+			<ContentGroup id="skip-catalog" group="catalog" v-model="setupData.project.catalog.type" value="skip_import">
 
 				<template #title>
 					Skip this step and continue
 				</template>
-				
+
 			</ContentGroup>
 		</div>
-		
+
 		<template #footer="{ advanceStep, retreatStep }">
 			<BackButton :action="retreatStep" />
 			<NextButton :action="advanceStep" />
@@ -79,9 +81,15 @@
 	import NextButton from '../Elements/NextButton.vue'
 	import Step from '../Step.vue'
 	import SuccessMessage from '../Elements/SuccessMessage.vue'
+  import ErrorMessage from '../Elements/ErrorMessage.vue'
 	import WarningMessage from '../Elements/WarningMessage.vue'
 	import ContentGroup from '../Elements/ContentGroup.vue'
 	import { PhotographIcon } from '@heroicons/vue/outline'
+  import {inject, ref} from "vue";
+
+  const setupData = inject('setupData')
+  const advanceStep = inject('advanceStep')
+  const errorMessage = ref(null)
 
 	export default {
 		components: {
@@ -89,9 +97,33 @@
 			NextButton,
 			Step,
 			SuccessMessage,
+      ErrorMessage,
 			WarningMessage,
 			ContentGroup,
 			PhotographIcon
 		},
+
+    setup() {
+      const setupData = inject('setupData')
+      const advanceStep = inject('advanceStep')
+      const errorMessage = ref(null)
+
+      const attemptAdvance = () => {
+        errorMessage.value = null
+
+        if (selectedTheme === null) {
+          errorMessage.value = 'You must choose a theme before proceeding.'
+
+          return;
+        }
+
+        return advanceStep()
+      }
+
+      return {
+        errorMessage,
+        setupData,
+      }
+    }
 	}
 </script>
--- app/src/project.js
+++ app/src/project.js
@@ -29,4 +29,7 @@ export default {
         frameworks: '',
     },
     storeName: '',
+    catalog: {
+        type: 'import',
+    }
 }
